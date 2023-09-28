9e765606bf8ec2c30117e523764d1ca94a3358a4
devops <devops@kaniyam.hashlabs.in>
2023-09-27 23:43:21 -0400

    build updated

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+FROM ubuntu
+RUN apt update
+RUN apt install nginx -y
+COPY index.html /var/www/html/
+EXPOSE 80
+CMD ["nginx", "-g","daemon off;"]
+
--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 git clone https://github.com/brindhasd/devops_Project2.git
 cd devops_Project2
-docker pull busybox
-docker run busybox -p 8080:80 busybox
-
-
+#docker pull busybox#
+docker build -t brindhasd/bluelagon .
+docker push brindhasd/bluelagon 
+docker run -d -p 8080:80 brindhasd/bluelago
